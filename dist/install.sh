fe2a152147e8bd4e0094629d66cc3b6c8ed28acf
rdenson <richardtdenson@gmail.com>
2021-10-08 09:45:28 -0500

    killing the dist directory and attempting to use goreleaser

--- dist/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GOOS=darwin \
-GOARCH=amd64 \
-go build \
-  -ldflags="-s -w -X 'github.com/rdenson/rtime/cmd.ToolVersion=$(git rev-parse --short HEAD)'"
-
-./rtime completion bash > /usr/local/etc/bash_completion.d/rtime
