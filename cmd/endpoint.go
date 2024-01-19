2803f2c7f04361ebc937d5cb7298687d84699a96
rdenson <richardtdenson@gmail.com>
2024-01-19 10:45:27 -0600

    removing comments

--- cmd/endpoint.go
+++ cmd/endpoint.go
@@ -7,40 +7,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-/*
-	var endpointCmd = &cobra.Command{
-		Use:   "endpoint [url]",
-		Short: "request a specific resource",
-		Long: `Attempts to request the specified URL. Does not inspect the response
-	  body.`,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			req, newRequestErr := resource.NewRequest(
-			// resource.OptionRequestUrl(args[0]),
-			)
-			if newRequestErr != nil {
-				return newRequestErr
-			}
-
-			req.SetRedirectsToPrint()
-			reqResult := req.Exec()
-			reqResult.PrettyPrint()
-			if reqResult.Err != nil {
-				return reqResult.Err
-			}
-
-			fmt.Printf("status: %d\n", reqResult.Status)
-			if showHeaders, _ := cmd.Flags().GetBool("show-headers"); showHeaders {
-				showResponseHeaders(reqResult.Response.Header)
-			}
-
-			if analyzeTls, _ := cmd.Flags().GetBool("analyze-tls"); analyzeTls {
-				showResponseTlsInfo(reqResult.Response.TLS)
-			}
-
-			return nil
-		},
-	}
-*/
 var endpointCmd = &cobra.Command{
 	Use:   "endpoint [url]",
 	Short: "request a specific resource",
